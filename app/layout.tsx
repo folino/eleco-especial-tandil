c18a21f130b072b1121dfeee0aabcb35b3342d02
Leandro Folino <leanrofolino@gmail.com>
2026-03-22 06:03:48 -0300

    clar

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,15 +30,8 @@ export default function RootLayout({
     <html lang="es">
       <body>
         <Script
-          id="clarity"
+          src="https://www.clarity.ms/tag/vzo26ivjml"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "vzo26ivjml");`,
-          }}
         />
         <LenisProvider>
           <Header />
